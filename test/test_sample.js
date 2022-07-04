88efeb0aa560a8bb772e4eb9feb80f879f3ce0cf
Jeet Jain <Jeet88833@gmail.com>
2022-07-04 15:18:54 +0530

    Fix for failing NPM 16 build

---
+++ test/test_sample.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+
+describe("Basic tests for Navigator", () => {
+	before(() => {
+		console.log("Starting navigation tests");
+	});
+
+	after(() => {
+		console.log("Basic tests completed");
+	});
+
+
+	describe("Status Bar Test", () => {
+		it("Status bar navigation forward is successful", () => {
+			assert.equal(2 + 3, 5);
+		});
+
+		it("Status bar navigation back is successful", () => {
+			assert.equal(2 * 3, 6);
+		});
+	});
+
+	describe("Menu Bar Test", () => {
+		it("Menu bar navigation forward is successful", () => {
+			assert.equal(2 + 3, 5);
+		});
+
+		it("Menu bar navigation back is successful", () => {
+			assert.equal(2 * 4, 8);
+		});
+	});
+});
